8e6c88f733d2080690a8f27523ef08693485408f
Doan Hanh Dep Trai <doanxhanh05@gmail.com>
2025-10-12 18:40:06 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,20 @@
+// class component
+// function component
+import React from 'react'
+
+class MyComponent extends React.Component {
+    // JSX
+    render() {
+        return (
+
+
+
+            <div>my first cmoponent
+                {
+                    Math.random()
+                }
+            </div>
+        );
+    }
+}
+export default MyComponent;
