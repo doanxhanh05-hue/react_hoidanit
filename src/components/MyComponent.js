90a41a41b7709bc54e676d1bdfc52df271c03577
Doan Hanh Dep Trai <doanxhanh05@gmail.com>
2025-10-13 21:18:58 +0700

    video17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,17 +3,19 @@
 import React from 'react'
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Hanh xau nhat the gioi',
+        address: 'hoi dan it',
+        age: 24,
+    };
+
     // JSX
     render() {
         return (
-
-
-
-            <div>my first cmoponent
-                {
-                    Math.random()
-                }
+            <div>
+                my name is{this.state.name} and i from {this.state.address}
             </div>
+            // this =>dang su dung mycomponent
         );
     }
 }
