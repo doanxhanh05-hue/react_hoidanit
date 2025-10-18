97023706458d52196d4d08c3bb754764a6abfb24
Doan Hanh Dep Trai <doanxhanh05@gmail.com>
2025-10-19 03:23:16 +0700

    video20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,13 +5,20 @@ import React from 'react'
 class MyComponent extends React.Component {
     state = {
         name: 'Hanh xau nhat the gioi',
-        address: 'hoi dan it',
-        age: 24,
+        // address: 'hoi dan it',
+        // age: 24,
     };
     handleClick(event) {
         console.log(">> click me my button");
-        console.log('myname is hanh');
+        console.log('random', Math.floor((Math.random() * 100) + 1));
 
+        this.setState({
+            name: 'eric',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+        // this.setState({
+        //     age: Math.floor((Math.random() * 100) + 1)
+        // })
     }
     handleOnMoverOver(event) {
         console.log(event.pageX);
@@ -20,9 +27,9 @@ class MyComponent extends React.Component {
     render() {
         return (
             <div>
-                my name is{this.state.name} and i from {this.state.address}
+                my name is{this.state.name} and i'm {this.state.age}
                 <button onMouseOver={this.handleOnMoverOver}>hover me</button>
-                <button onClick={this.handleClick}>click me</button>
+                <button onClick={(event) => { this.handleClick(event) }}>click me</button>
             </div>
             // this =>dang su dung mycomponent
 
