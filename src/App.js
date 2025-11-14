564bd2e5ad242d36fd7b411cefff92ffc98cdce5
Hanh Le Doan <doanxhanh05@gmail.com>
2025-11-14 15:08:39 +0700

    'video47'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss';
-
+import Header from './components/header/Header';
 const App = () => {
 
   return (
     <div className="app--container">
-      helloword
+      <Header></Header>
 
     </div>
   );
